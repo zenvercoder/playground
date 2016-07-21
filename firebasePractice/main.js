444a0386524dbf7a0cc456a4a8c271a4e593f677
Lisa Hull <lisa@jchull.com>
2016-07-20 20:53:50 -0600

    touch ups, more punchlines + gifs added

--- firebasePractice/main.js
+++ firebasePractice/main.js
@@ -3,6 +3,21 @@ var punchline = [
     "Did I do that?",
     "yo momma!",
     "clap along if you feel like a room without a roof!",
+    "I’ll have what she’s having.",
+    "That's when I developed my drinking problem",
+    "You're supposed to stop inserting the q-tip when you feel resistance",
+    "And I’m Ron Burgundy. Go fuck yourself, San Diego.",
+    "It’s just a flesh wound.",
+    "I’m letting you off easy! I was going to ask for the whole wig.",
+    "I really don’t care for the way you’re speaking to me.",
+    "Well, there goes your social life",
+    "We are the knights who say ni!!",
+    "She turned me into a newt... I got better",
+    "That rabbit's got a vicious streak a mile wide! It's a killer!",
+    "Oh! Now we see the violence inherent in the system! Help, help, I'm being repressed!",
+    "Bring out your dead!"
+
+
 
 ];
 // primary purpose of a function: to return something
@@ -16,10 +31,25 @@ $(".btn-1").click(function(){
 
 
 var imageArray = [
-    "http://67.media.tumblr.com/4d430e086ecaffa7e98c8b3b42a6ca24/tumblr_ndxpmdQX9C1rbibvmo1_1280.jpg",
     "https://i.ytimg.com/vi/d_rC9c200wg/hqdefault.jpg",
     "https://pbs.twimg.com/profile_images/3153378667/5bbfbbb82568cc1f8b468268a1acf985_400x400.jpeg",
     "https://s-media-cache-ak0.pinimg.com/236x/f0/f8/f5/f0f8f5889fb6ff45e802cf33201177c5.jpg",
+    "http://i.giphy.com/PqM1wJBvvIBZS.gif",
+    "http://i.giphy.com/dDArd5rbZ1alq.gif",
+    "http://i.giphy.com/YiNtOZm6srLpe.gif",
+    "http://i.giphy.com/JfvdoqEyVHb3y.gif",
+    "http://i.giphy.com/ovt5W1f1EuYyA.gif",
+    "http://i.giphy.com/ItWzWnWSjPsbe.gif",
+    "http://i.giphy.com/WAazKNEk4s0Ug.gif",
+    "http://i.giphy.com/pg3D0DkUGxBug.gif",
+    "http://i.giphy.com/N8Lfh9gWcWYIU.gif",
+    "http://i.giphy.com/Vpdzoviaa1P3y.gif",
+    "http://i.giphy.com/euqFD0jqYA0bS.gif",
+    "http://i.giphy.com/xThuWdkfrKQ64eSToA.gif",
+    "http://i.giphy.com/ZrGJNAD4A7kXu.gif",
+    "http://i.giphy.com/eOI6bXMKsK0ow.gif",
+    "http://i.giphy.com/Lw4f38T1CuQla.gif",
+    "http://i.giphy.com/N0wCfSOVtX7PO.gif",
 ];
 // primary purpose of a function: to return something
 function generateImage(){
