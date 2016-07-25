7b19d72ec948a9594031137cbff743956d7fee70
Lisa Hull <lisa@jchull.com>
2016-07-25 10:14:41 -0600

    roles populated in dropdown menu

--- personnel/main.js
+++ personnel/main.js
@@ -5,6 +5,9 @@ var value;
 var lastNameValue;
 var role;
 var newImage;
+var option;
+
+var roles;
 
 $("#firstName").on("keyup", function(event){
     value = $(this).val();
@@ -35,11 +38,29 @@ $.ajax({
     "type": "GET",
 
     success: function(data) {
-        for (var item of data) {
-            console.log("item.title= " + item.title);
+        //make sure it's not undefined with if statement
+        if(data){
+            $('select').append("<option value='selectOption'>Select an option here</option>");
+            //create object to save data to own object
+            roles = data;
+            for(key in data){
+                console.log(data[key].title)
+                var roleTitle = data[key].title;
+                var roleImage = data[key].img;
+                console.log("data[key].img= " , data[key].img)
+                $('select').append("<option value='selectOption'>" + data[key].title + "</option>");
+
+            //value is how you determine which option you've selected
+            //    used the key and put that in the value that will be the position of the object
+            }
+        //can append variable options to select
+        //    any time you take a drop down, a change event is fired.
+        //    broke out of ajax call made two extra functions.
         }
     }
 });
+
+
 
 
 
